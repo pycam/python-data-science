75804f42e1f7b2c5a6f04aa8588c6806f858d852
pajanne <pajanne@gmail.com>
2017-03-27 16:38:27 +0100

    Much better solution using a filter function and the csv module.

--- solutions/ex4_1.py
+++ solutions/ex4_1.py
@@ -1,22 +1,21 @@
+import csv
+
 # initialise variables
-life_expectancy_data_old = []
-gdp_per_capita_data_old = []
+dataset = []
 
-life_expectancy_data_new = []
-gdp_per_capita_data_new = []
-
-# read GapMinder dataset from file and filter the years of interest
+# read GapMinder dataset from file and filter the years of interest using CSV
 with open('data/gapminder.txt') as f:
-    for line in f:
-        country, continent, year, life_expectancy, pop, gdp_per_capita = line.strip().split('\t')
-        if country == 'country':
-            continue
-        if year == '1957':
-            life_expectancy_data_old.append(float(life_expectancy))
-            gdp_per_capita_data_old.append(float(gdp_per_capita))
-        elif year == '2007':
-            life_expectancy_data_new.append(float(life_expectancy))
-            gdp_per_capita_data_new.append(float(gdp_per_capita))
+    reader = csv.DictReader(f, delimiter='\t')
+    for record in reader:
+        dataset.append(record)
+
+# define function to filter the dataset based on year and extract one column
+def filter_dataset(dataset, column_name, year):
+    results = []
+    for data in dataset:
+        if data['year'] == year:
+            results.append(data[column_name])
+    return results
 
 
 # scatter plots
@@ -24,8 +23,8 @@ import matplotlib.pyplot as mpyplot
 mpyplot.xlabel('Life Expectancy')
 mpyplot.ylabel('GDP per Capita')
 mpyplot.title('GapMinder world data over 50 years')
-mpyplot.scatter(life_expectancy_data_old, gdp_per_capita_data_old, c='b', label='1957')
-mpyplot.scatter(life_expectancy_data_new, gdp_per_capita_data_new, c='g', label='2007')
+mpyplot.scatter(filter_dataset(dataset, 'lifeExp', '1957'), filter_dataset(dataset, 'gdpPercap', '1957'), c='b', label='1957')
+mpyplot.scatter(filter_dataset(dataset, 'lifeExp', '2007'), filter_dataset(dataset, 'gdpPercap', '2007'), c='g', label='2007')
 mpyplot.legend()
 mpyplot.grid(True)
 mpyplot.show()
